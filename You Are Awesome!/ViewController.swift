4aac1920cd86da259b19316af2bb1f33e07b030d
Mina Robinson <minayoo@comcast.net>
2021-09-07 16:47:24 -0700

    ch 2.12 -> Before send mac away

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber=0
+    var messageNumber=0
+    let totalNumberOfImages=9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +23,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages=["You Are Awesome!",
+                      "You Are Great!",
+                      "You Are Fantastic!",
+                      "When the Genius Bar Needs Help, They Call You!",
+                      "Fabulous? That's You!",
+                    "You've Got The Design Skills of Jony Ive"]
+        messageLabel.text=messages[Int.random(in:0...messages.count-1)]
+        imageView.image=UIImage(named:"image\(Int.random(in:0...totalNumberOfImages))")
         
-        print(imageNumber)
-        let imageName="image" + String(imageNumber)
-        imageView.image=UIImage(named:imageName)
-        imageNumber=imageNumber+1
-        if imageNumber==10 {
-            imageNumber=0
-        }
         
+        
+        
+//        messageLabel.text=messages[messageNumber]
+//        messageNumber+=1
+//        if messageNumber==messages.count{
+//            messageNumber=0
+//        }
+        
+//        let imageName="image" + String(imageNumber)
+//        imageView.image=UIImage(named:imageName)
+//        imageNumber=imageNumber+1
+//        if imageNumber==10 {
+//            imageNumber=0
+//        }
+//
       
         
 //        let awesomeMessage="You Are Awesome!"
