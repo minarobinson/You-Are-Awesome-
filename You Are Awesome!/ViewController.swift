45f4a135c82d425c4a63e471eaa73168f7c0b617
Mina Robinson <minayoo@comcast.net>
2021-09-10 12:32:14 -0700

    New You Are Awesome  from Library

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -34,37 +34,7 @@ class ViewController: UIViewController {
         
         
         
-        
-//        messageLabel.text=messages[messageNumber]
-//        messageNumber+=1
-//        if messageNumber==messages.count{
-//            messageNumber=0
-//        }
-        
-//        let imageName="image" + String(imageNumber)
-//        imageView.image=UIImage(named:imageName)
-//        imageNumber=imageNumber+1
-//        if imageNumber==10 {
-//            imageNumber=0
-//        }
-//
-      
-        
-//        let awesomeMessage="You Are Awesome!"
-//        let greatMessage="You Are Great!"
-//        let bombMessage="You Are Da Bomb!"
-//
-//        if messageLabel.text==awesomeMessage{
-//            messageLabel.text=greatMessage
-//            imageView.image=UIImage(named:"image1")
-//        }else if messageLabel.text==greatMessage{
-//            messageLabel.text=bombMessage
-//            imageView.image=UIImage(named:"image2")
-//        }
-//        else {
-//            messageLabel.text=awesomeMessage
-//            imageView.image=UIImage(named:"image0")
-//        }
+
     }
     
 }
